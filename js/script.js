720a1ab6575ee97d0898690153271c9fee9b8c4f
Hilton Muccillo <hmuccillo@gmail.com>
2023-11-13 14:35:33 -0300

    Update fade

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 (function(){
     setTimeout(function(){
-        document.querySelector(".author").style.cssText = "opacity: 1";
-        document.querySelector(".effect").style.cssText = "display: none";                      
+        document.querySelector(".author").style.cssText = "opacity: 1";                   
     }, 4500);
+    setTimeout(function(){
+        document.querySelector(".effect").style.cssText = "display: none";                      
+    }, 6500);
 })();
