58f5135f169903d0b9d966cbaed3b49d396848f6
Hilton Muccillo <hmuccillo@gmail.com>
2023-11-13 10:49:02 -0300

    Update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+(function(){
+    setTimeout(function(){
+        document.querySelector(".author").style.cssText = "opacity: 1";
+        document.querySelector(".effect").style.cssText = "display: none";                      
+    }, 4500);
+})();
